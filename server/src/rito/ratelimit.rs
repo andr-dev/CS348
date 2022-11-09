828dcfcb30709b2b52cbfd19324fb140d83782ed
andrthesnek <andr@the.snek>
2022-11-09 18:57:11 -0500

    implement rate limiter

--- server/src/rito/ratelimit.rs
+++ server/src/rito/ratelimit.rs
@@ -1,22 +1,34 @@
-use std::collections::VecDeque;
-use tokio::time::Instant;
+use std::sync::{Arc, Mutex};
+use std::time::{Duration, Instant};
 
 pub struct RateLimiter {
-    track: VecDeque<Instant>,
-    count: usize,
-    duration: usize,
+    track: Arc<Mutex<Instant>>,
+    count: u64,
+    duration: u64,
 }
 
 impl RateLimiter {
-    pub fn new(count: usize, duration: usize) -> Self {
+    pub fn new(count: u64, duration: u64) -> Self {
         Self {
-            track: VecDeque::new(),
+            track: Arc::new(Mutex::new(Instant::now())),
             count,
             duration
         }
     }
 
     pub async fn wait(&self) {
+        loop {
+            let mut lock = self.track.lock().unwrap();
 
+            let secs = lock.elapsed().as_secs();
+
+            if secs > self.duration {
+                *lock = Instant::now();
+                break;
+            } else {
+                drop(lock);
+                tokio::time::sleep(Duration::from_secs(self.duration - secs));
+            }
+        }
     }
 }
